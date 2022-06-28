175cf06ed3093daeb5601819d9d44fa3d5a44c3b
Leo Singer <leo.singer@ligo.org>
2022-06-28 15:15:44 -0400

    Fix prometheus.yml path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-printf "%s" "${PROMETHEUS_CONFIG_CONTENT}" > /etc/prometheus/prometheus.yml
+printf "%s" "${PROMETHEUS_CONFIG_CONTENT}" > /prometheus/prometheus.yml
 exec /bin/prometheus $@
